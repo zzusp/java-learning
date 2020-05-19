02f8ef1fdb95afa2292b1eac861cc910e4023cc1
peng <123456>
2020-05-19 19:13:03 +0800

    线程状态知识点改正

--- concurrency/thread/src/main/java/Main.java
+++ concurrency/thread/src/main/java/Main.java
@@ -7,21 +7,21 @@
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
-		// 新建状态（New）
+		// 初始状态（NEW）
 		Thread thread = new Thread(() -> {
-			// 阻塞状态（Blocked），阻塞两秒
+			// 超时等待状态（TIME_WAITING），等待两秒
 			try {
 				Thread.sleep(2000L);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			// 运行状态（Running）
+			// 运行状态（RUNNING）
 			System.out.println(Thread.currentThread().getName() + " is running");
-			// 执行结束，死亡（Dead）
+			// 执行结束，死亡（TERMINATED）
 		});
-		// 就绪状态（Runnable）
+		// 运行状态（RUNNABLE）
 		thread.start();
-		// 阻塞状态（Blocked），join()等待线程终止或者超时，然后转为就绪状态
+		// 等待状态（WAITING），join()等待线程终止或者超时，然后转为就绪状态
 		thread.join();
 	}
 
