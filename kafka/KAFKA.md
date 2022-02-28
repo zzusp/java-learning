ff581af769cb580add2628de39745f669c33528c
孙鹏 <peng.sun@aiseeding.com>
2022-02-28 17:41:14 +0800

    kafka生产者及broker

--- kafka/KAFKA.md
+++ kafka/KAFKA.md
@@ -1,13 +1,49 @@
 # kafka
+* [快速启动](./QUICKSTART.md)
 
+## 术语
+* AR：分区中的所有副本统称为AR（Assigned Replicas）。
+* ISR：所有与leader副本保持一定程度同步的副本（包括leader副本在内）称为ISR（In-sync Replicas）。ISR集合是AR的一个子集，
+并且可以看作是与leader副本数据比较同步的副本。
+* OSR：与leader副本同步滞后过多的副本（不包括leader副本）称为OSR（Out-sync Replicas）。OSR集合也是AR的一个子集。OSR是与ISR互斥的。
+* LEO（Log End Offset）：每个副本的最后一个offset，LEO其实就是最新的offset+1
+* HW（High Watemark）：所有副本中最小的LEO
 
 ## Topic
 * topic
-    * partition
-        * segment（Partition 全局的第一个 Segment 从 0 开始，后续每个 Segment 文件名为上一个 Segment 文件最后一条消息的 offset 值。数值最大为 64 位 long 大小，19 位数字字符长度，没有数字用0填充。如 00000000000000368769.index 和 00000000000000368769.log。）
-            * index file(.index)
-                
+    * partition（命名规则，topic+分区号）
+        * segment（Partition全局的第一个Segment从0开始，后续每个Segment文件名为文件中第一条消息的offset值。
+        数值最大为64位long大小，19位数字字符长度，没有数字用0填充。如00000000000000368769.index和00000000000000368769.log）
+            * index file(.index)（稀疏索引，每4kb消息插入一条索引）
+                * 相对offset：相对于当前索引文件的偏移量
+                * position：message 的物理地址
             * data file(.log)
-                * offset
-                * MessageSize
-                * data
+                * offset：表示Message在这个partition中的偏移量
+                * MessageSize：消息内容data的大小
+                * data：data为Message的具体内容
+            * 时间戳索引(.timeindex)
+            * 其他
+            * 查看.index/.log/.timeindex文件，可用命令：kafka-run-class.sh kafka.tools.DumpLogSegments --files 文件路径/xxxx.index
+
+## Follower故障及恢复场景
+* Follower发生故障后会被临时踢出ISR（进入OSR）
+* 这个期间Leader和正常的Follower继续接收数据
+* 待故障Follower恢复后，Follower会读取本地磁盘记录的上次的HW，并将log文键高于HW的部分截取掉，从HW开始向Leader进行同步
+* 等到故障Follower的LEO大于等于该Partition的HW，即Follower追上Leader之后，就可以重新加入ISR了
+
+## Leader故障
+* Leader发生故障之后，会从AR中，按照AR顺序从第一个副本开始，选出一个存在于ISR中的副本（即存活的）作为新的Leader
+* 为保证多个副本之间的数据一致性，其余的Follower会先将各自的log文件高于HW的部分截掉，然后从新的Leader同步数据
+
+注：这只能保证副本之间的数据一致性，并不能保证数据不丢失或不重复
+
+## 高效读写数据
+* kafka本身是分布式集群，可以采用分区技术，并行度高
+* 读数据采用稀疏索引，可以快速定位要消费的数据
+* 顺序写磁盘。写入方式为在文件末端追加，省去大量磁头寻址（机械硬盘）的时间。官方数据表明：同样的磁盘，顺序写能到600M/s，随机写只有100K/s
+* 页缓存（PageCache）。linux利用空闲内存，缓存最近使用的硬盘数据，从而减少磁盘I/O操作,提高性能
+* 零拷贝技术（完全依赖于操作系统是否支持）
+    * kafka集群接收到consumer的请求
+    * 调用系统内核拉取消息（从PageCache或磁盘）
+    * 系统内核直接返回消息到consumer端（不需要再将数据拷贝到kafka集群，然后由kafka集群返回）
+
--- kafka/kafka-demo/src/main/java/kafka/Producer.java
+++ kafka/kafka-demo/src/main/java/kafka/Producer.java
@@ -15,48 +15,94 @@ import java.util.Properties;
  * @modified By
  */
 public class Producer {
-	private static final String TOPIC = "milo2";
-	private static final String BROKER_LIST = "127.0.0.1:9092";
-	private static KafkaProducer<Integer, String> producer = null;
+    private static final String TOPIC = "milo2";
+    private static final String BROKER_LIST = "127.0.0.1:9092";
+    private static KafkaProducer<Integer, String> producer;
 
-	/*
-	初始化生产者
-	 */
-	static {
-		Properties configs = initConfig();
-		producer = new KafkaProducer<>(configs);
-	}
+    /*
+    初始化生产者
+     */
+    static {
+        Properties configs = initConfig();
+        producer = new KafkaProducer<>(configs);
+    }
 
-	/*
-	初始化配置
-	 */
-	private static Properties initConfig() {
-		Properties properties = new Properties();
-		properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BROKER_LIST);
-		properties.put(ProducerConfig.ACKS_CONFIG, "all");
-		properties.put(ProducerConfig.RETRIES_CONFIG, 0);
-		properties.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
-		properties.put(ProducerConfig.LINGER_MS_CONFIG, 1);
-		properties.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
-		properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
-		properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		return properties;
-	}
+    /*
+    初始化配置
+     */
+    private static Properties initConfig() {
+        Properties properties = new Properties();
+        // broker地址配置
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BROKER_LIST);
+        // 重试次数。默认0
+        properties.put(ProducerConfig.RETRIES_CONFIG, 0);
+        // 键的序列化类配置
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+        // 值的序列化类配置
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
-	public static void main(String[] args) {
-		// 消息实体
-		ProducerRecord<Integer, String> record = null;
-		for (int i = 0; i < 10; i++) {
-			record = new ProducerRecord<>(TOPIC, i, "value" + (int) (10 * (Math.random())));
-			// 发送消息
-			producer.send(record, (recordMetadata, e) -> {
-				if (null != e) {
-					System.out.println(e.getMessage());
-				} else {
-					System.out.println(String.format("offset:%s,partition:%s", recordMetadata.offset(), recordMetadata.partition()));
-				}
-			});
-		}
-		producer.close();
-	}
+        // ---------------- 吞吐量提高 ----------------
+
+        // 每批消息的最大值，单位字节。默认16384（int），达到最大值时发送该批消息
+        properties.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        // 发送消息的时间间隔，单位毫秒。每隔一段时间就发送一批消息，一般不超过100ms
+        properties.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        // 压缩类型，选项：'gzip', 'snappy', 'lz4', 'zstd'。默认producer，其意思是保留由生产者设置的原始压缩编解码器
+        properties.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "producer");
+        // 待发送消息总大小限制，单位字节。默认33554432（long），超出限制，则抛出异常
+        properties.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432L);
+
+        // ---------------- 保证消息不丢失也不重复 ----------------
+
+        // broker的leader的响应配置，all（-1等同all）表示将等待全套同步副本（ISR）确认记录。默认all
+        properties.put(ProducerConfig.ACKS_CONFIG, "all");
+        // 开启幂等性。默认true
+        // 开启幂等性后，发送消息时会包含<ProducerID,Partition,SequenceNumber>这三个信息，根据这三个信息来判断消息是否重复
+        // ProducerID：在每个新的Producer初始化时，会被分配一个唯一的ProducerID，这个ProducerID对客户端使用者是不可见的。
+        // （通过org.apache.kafka.clients.producer.internals.Sender#maybeWaitForPid方法生成ProducerID）
+        // Partition：分区号
+        // SequenceNumber：对于每个ProducerID，Producer发送数据的每个Topic和Partition都对应一个从0开始单调递增的SequenceNumber值。
+        properties.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+        // ProducerID重启就会变化，同时不同的Partition也具有不同主键，所以幂等性无法保证跨分区跨会话的Exactly Once。所以需要引入事务。
+        // 事务ID，需全局唯一
+        properties.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "xxxxx");
+
+        // ---------------- 保证消息有序 ----------------
+
+        // 在阻塞之前，客户端在单个连接上发送的最大未确认请求数。默认5
+        // 如果为1则不需要判断是否开启幂等性，即可保证消息有序
+        // 如果<=5并且开启幂等性，也可保证消息有序；如果>5不保证消息有序。（每个broker最多缓存5个请求）
+        properties.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 5);
+
+        return properties;
+    }
+
+    public static void main(String[] args) {
+        producer.initTransactions();
+
+        producer.beginTransaction();
+
+        try {
+            // 消息实体
+            ProducerRecord<Integer, String> record;
+            for (int i = 0; i < 10; i++) {
+                record = new ProducerRecord<>(TOPIC, i, "value" + (int) (10 * (Math.random())));
+                // 同步发送（阻塞，直到消息发送完成）：send(xxx).get();send(xxx,callback).get();
+                // 异步发送：send(xxx);send(xxx,callback);
+                // 发送消息
+                producer.send(record, (recordMetadata, e) -> {
+                    if (null != e) {
+                        System.out.println(e.getMessage());
+                    } else {
+                        System.out.println(String.format("offset:%s,partition:%s", recordMetadata.offset(), recordMetadata.partition()));
+                    }
+                });
+            }
+            producer.commitTransaction();
+        } catch (Exception e) {
+            producer.abortTransaction();
+        } finally {
+            producer.close();
+        }
+    }
 }
